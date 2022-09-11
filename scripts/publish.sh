c0986dac63aed8ee5a982f682b794ba13d7d0cee
CryptoNate <89227631+DeFiDruid@users.noreply.github.com>
2022-09-11 05:10:30 +0100

    added some shell scripts for tasks

---
+++ scripts/publish.sh
@@ -0,0 +1,6 @@
+## Fund account with some devnet Aptos Coins from faucet
+aptos account fund-with-faucet --account 0xedf2c8addd8c2d48f3975fc6171c5b654d730688036115bdb5746e2c791046e5
+
+## Publish all move modules within the package
+
+aptos move publish --profile default
